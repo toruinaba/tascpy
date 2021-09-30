d83ad280bdf433b1f5d25e01b3e744b5cc230349
inaba <inaba.tooru@takenaka.co.jp>
2021-09-30 22:27:42 +0900

    Add/Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="tascpy",
+    version="0.0.1",
+    description="",
+    author="1500197",
+    author_email="",
+    url="",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+)
