d6e7c1d98512e1591f4edf0b89511ec3d9369383
toruinaba <inaba.tooru@takenaka.co.jp>
2025-05-06 14:02:40 +0900

    Modify/import scripts in core.__init__

--- src/tascpy/operations/__init__.py
+++ src/tascpy/operations/__init__.py
@@ -1,3 +1,9 @@
 from .registry import operation, OperationRegistry
 
 from .core import filters
+from .core import math
+from .core import search
+from .core import plot
+from .core import transform
+from .core import interpolate
+from .core import split
