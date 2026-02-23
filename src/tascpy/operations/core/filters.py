2ec36bd80774dac27967e4260a836f0a6972c915
toruinaba <inaba.tooru@takenaka.co.jp>
2026-02-23 10:16:14 +0900

    Refactor: Merge filter_utils.py back into filters.py

--- src/tascpy/operations/core/filters.py
+++ src/tascpy/operations/core/filters.py
@@ -4,15 +4,15 @@ from ...core.column import Column
 from ...core.step import Step
 from ..registry import operation, register_functional, register_pipeline
 from ..abstraction import inject_columns, filter_rows, inject_step_values
-from ...functional import filter_utils, stats as functional_stats
-from ...functional import filters as functional_filters
+from ...functional import filters, stats as functional_stats
+from ...functional import filters as filters
 import inspect
 import numpy as np
 
 
 # Refactored to use functional core
 filter_by_value = register_functional(
-    filter_utils.eq,
+    filters.eq,
     domain="core",
     name="filter_by_value",
     inject_columns={"num_inputs": 1, "cast_to_numpy": True},
@@ -27,7 +27,7 @@ filter_by_value = register_functional(
 
 
 filter_out_none = register_functional(
-    filter_utils.filter_valid_rows,
+    filters.filter_valid_rows,
     domain="core",
     name="filter_out_none",
     inject_columns={"columns_arg": "columns", "cast_to_numpy": True},
@@ -40,7 +40,7 @@ filter_out_none = register_functional(
 
 
 remove_consecutive_duplicates_across = register_functional(
-    filter_utils.duplicated_indices,
+    filters.duplicated_indices,
     domain="core",
     name="remove_consecutive_duplicates_across",
     inject_columns={"columns_arg": "columns", "cast_to_numpy": True},
@@ -57,7 +57,7 @@ remove_consecutive_duplicates_across = register_functional(
 remove_outliers = register_pipeline(
     steps=[
         (functional_stats.detect_outliers, {}),
-        (filter_utils.eq, {"value": 0}),
+        (filters.eq, {"value": 0}),
         (filter_rows, {})
     ],
     domain="core",
@@ -75,7 +75,7 @@ remove_outliers = register_pipeline(
 
 
 filter_by_condition = register_functional(
-    functional_filters.filter_by_condition,
+    filters.filter_by_condition,
     domain="core",
     name="filter_by_condition",
     filter_rows=True,
@@ -87,7 +87,7 @@ filter_by_condition = register_functional(
 
 
 remove_steps = register_functional(
-    functional_filters.remove_steps_mask,
+    filters.remove_steps_mask,
     domain="core",
     name="remove_steps",
     filter_rows=True,
@@ -103,7 +103,7 @@ remove_steps = register_functional(
 # ---------------------------------------------------------
 
 search_by_value = register_functional(
-    filter_utils.search,
+    filters.search,
     domain="core",
     name="search_by_value",
     inject_columns={"num_inputs": 1, "cast_to_numpy": True},
@@ -114,7 +114,7 @@ search_by_value = register_functional(
 
 
 search_by_range = register_functional(
-    filter_utils.search_range,
+    filters.search_range,
     domain="core",
     name="search_by_range",
     inject_columns={"num_inputs": 1, "cast_to_numpy": True},
@@ -140,7 +140,7 @@ def _search_step_metadata(args, kwargs, result):
 
 
 search_by_step_range = register_functional(
-    filter_utils.search_step_range,
+    filters.search_step_range,
     domain="core",
     name="search_by_step_range",
     inject_step_values={"cast_to_numpy": True},
@@ -149,7 +149,7 @@ search_by_step_range = register_functional(
 
 
 search_by_condition = register_functional(
-    functional_filters.search_by_condition,
+    filters.search_by_condition,
     domain="core",
     name="search_by_condition",
     inject_columns={"columns_arg": "columns", "cast_to_numpy": True},
@@ -160,7 +160,7 @@ search_by_condition = register_functional(
 
 
 search_missing_values = register_functional(
-    functional_filters.search_missing_values,
+    filters.search_missing_values,
     domain="core",
     name="search_missing_values",
     inject_columns={"columns_arg": "columns", "cast_to_numpy": True},
@@ -171,7 +171,7 @@ search_missing_values = register_functional(
 
 
 search_top_n = register_functional(
-    filter_utils.top_n,
+    filters.top_n,
     domain="core",
     name="search_top_n",
     inject_columns={"num_inputs": 1, "cast_to_numpy": True},
